68fa452555f7041eec11e09e3d5ff7333d0bedf7
anaabalbi <anaabalbi@gmail.com>
2022-01-04 16:56:59 -0300

    srooll evento busca js

--- src/java_script/controllers/navbar_controller.js
+++ src/java_script/controllers/navbar_controller.js
@@ -4,5 +4,6 @@ class NavbarController {
     const tema = model.rolaPagina();
     const view = new NavbarView();
     view.ficaTransparente(tema);
+    view.someBusca(tema);
   }
 }
--- src/java_script/views/navbar-view.js
+++ src/java_script/views/navbar-view.js
@@ -4,4 +4,11 @@ class NavbarView {
       ? $('nav').addClass('transparencia-navbar')
       : $('nav').removeClass('transparencia-navbar');
   }
+  someBusca(tema) {
+    if (tema == 'rolou') {
+      $('.motor-pesquisa').css('display', 'none');
+    } else {
+      $('.motor-pesquisa').css('display', 'flex');
+    }
+  }
 }
